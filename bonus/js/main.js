85b2892c1b17eef64f28ee2d2111d1ab27f87b3f
robertoAureli <r.aureli@protonmail.com>
2021-09-06 19:42:58 +0200

    Pulizia

---
+++ bonus/js/main.js
@@ -0,0 +1,16 @@
+// Ciao ragazzi,
+// Esercizio di oggi: Vue Hello
+// nome repo: vue-hello
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+let app = new Vue({
+    el: "#root",
+    data: {
+        title: "Hello VueJS"
+    }
+})
+
+console.log("ok")
